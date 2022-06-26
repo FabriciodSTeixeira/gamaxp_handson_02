03614ae335f1c873f28ff8d1b00da851e62ad74d
Fabricio <fabriciosilvateixeira@hotmail.com>
2022-06-26 20:00:35 -0300

    update 3: ajustes url admin, inicio ajuste pagina editar evento

--- js/admin.js
+++ js/admin.js
@@ -31,8 +31,8 @@ async function atualizaLista() {
                     <td>${shows[i].attractions}</td>
                     <td>
                         <a href="reservas.html" class="btn btn-dark">ver reservas</a>
-                        <a href="editar-evento.html/?${shows[i]._id}" class="btn btn-secondary">editar</a>
-                        <a href="excluir-evento.html/?${shows[i]._id}" class="btn btn-danger">excluir</a>
+                        <a href="editar-evento.html?id=${shows[i]._id}" class="btn btn-secondary">editar</a>
+                        <a href="excluir-evento.html?id=${shows[i]._id}" class="btn btn-danger">excluir</a>
                     </td>
                 </tr>
                 `
--- js/editar-evento.js
+++ js/editar-evento.js
@@ -1,1 +1,17 @@
-addEventListener("onload")
+let urlString = window.location;
+let url = new URL(urlString);
+let id = url.search.substring('?id='.length)
+
+async function getIdText(idUrl) {
+    let response = await fetch(`https://xp41-soundgarden-api.herokuapp.com/events/${idUrl}`)
+    let text = await response.json();
+
+    function autoFill() {
+        // let formEditar = document.querySelector("form")
+        // formEditar.innerHTML =
+
+
+    }
+    autoFill()
+}
+getIdText(id)
